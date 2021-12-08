194842a17d10682506e5c6d9631bb6b88146e2e7
Jonas Jacobs <Jonas.jacobs6475@stu.metc.edu>
2021-12-08 10:17:52 -0700

    WeSplitApp

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jonas Jacobs on 12/7/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
